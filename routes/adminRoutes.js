5fbe6b282dea2b9dbb3cebbeb2b2040b5c59264a
Samuel Vitor <samuz0789@gmail.com>
2026-06-13 16:34:52 -0300

    Mover gerenciamento da loja para painel staff

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -59,12 +59,7 @@ router.get("/usuarios/:id", renderAdminUserDetails);
 router.post("/usuarios/:id/role", updateAdminUserRole);
 router.post("/usuarios/:id/password", updateAdminUserTemporaryPassword);
 router.get("/pedidos", renderAdminOrders);
-router.get("/loja/produtos", renderAdminStoreProducts);
-router.get("/loja/produtos/novo", renderAdminStoreProductNew);
-router.post("/loja/produtos/novo", createAdminStoreProduct);
-router.get("/loja/produtos/:id/editar", renderAdminStoreProductEdit);
-router.post("/loja/produtos/:id/editar", updateAdminStoreProduct);
-router.post("/loja/produtos/:id/toggle", toggleAdminStoreProduct);
+
 
 // Exportação precisa vir ANTES de /pedidos/:id
 router.get("/pedidos/export/json", exportOrdersJson);
